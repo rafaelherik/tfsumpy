a5ede4ed81964307d3017ace6c9a40840a63fe40
Rafael Herik <rafaelherik@gmail.com>
2025-02-02 15:45:22 +0100

    Bump version to 0.0.3

--- tfsumpy/__init__.py
+++ tfsumpy/__init__.py
@@ -1,6 +1,6 @@
 from .plan_analyzer import LocalPlanAnalyzer
 from .resource import ResourceChange
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 __all__ = ['LocalPlanAnalyzer', 'ResourceChange']
